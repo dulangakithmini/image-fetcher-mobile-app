06f55b81b941182607b2c55246f03e7d11fc8eea
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-05 05:02:49 +0530

    Return all the widgets from build function that the App class is showing on the screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,8 @@ import 'package:flutter/material.dart';
 // Define a main function to run when the app starts
 void main() {
   // Create a text widget to show some text on the screen
-  var app = MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Let\'s see some images!'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print('Hi');
-        },
-        child: Icon(Icons.add),
-      ),
-    ),
-  );
+  var app =
 
 // Take that widget and get it on the screen
-  runApp(app);
+      runApp(app);
 }
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -4,5 +4,19 @@ import 'package:flutter/material.dart';
 // Create a class for custom widget. This should extend the StatelessWidget base class
 class App extends StatelessWidget {
   // Define a build method that returns the widgets that this widget will show
-  Widget build(context) {}
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Let\'s see some images!'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('Hi');
+          },
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
 }
