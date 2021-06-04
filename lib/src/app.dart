fa29d2fc10de11dbcaba80853842e6c330c138fc
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-05 04:39:32 +0530

    Create class App extending StatelessWidget

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,1 +1,7 @@
+// Import flutter helper library
+import 'package:flutter/material.dart';
 
+// Create a class for custom widget. This should extend the StatelessWidget base class
+class App extends StatelessWidget {}
+
+// Define a build method that returns the widgets that this widget will show
