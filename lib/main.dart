3f08e6092b6fa0b99009f56529ed0d40d71e38fc
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-04 22:32:23 +0530

    Create an instance of MaterialApp and pass a Text widget to the home parameter

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 // Import helper library from Flutter to get content on the screen
+import 'package:flutter/material.dart';
 
 // Define a main function to run when the app starts
 void main() {
   // Create a text widget to show some text on the screen
+  var app = MaterialApp(
+    home: Text('Hi there'),
+  );
 
 // Take that widget and get it on the screen
 }
