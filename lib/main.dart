13c1223b004a9c1ce89163c03147dce78f1cd055
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-04 22:10:10 +0530

    Create main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 // Import helper library from Flutter to get content on the screen
 
 // Define a main function to run when the app starts
-
-// Create a text widget to show some text on the screen
+void main() {
+  // Create a text widget to show some text on the screen
 
 // Take that widget and get it on the screen
+}
