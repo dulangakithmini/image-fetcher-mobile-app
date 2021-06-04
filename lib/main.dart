c44083812319fcb44f5e94b2866b7b66cebb5ba1
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-05 05:25:04 +0530

    Create a new instance of App class and pass that instance to runApp() in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 // Import helper library from Flutter to get content on the screen
 import 'package:flutter/material.dart';
 
+// Import app.dart
+import 'src/app.dart';
+
 // Define a main function to run when the app starts
 void main() {
   // Create a text widget to show some text on the screen
-  var app =
+  // var app =
 
 // Take that widget and get it on the screen
-      runApp(app);
+//       runApp(app);
+
+// Takes the App class, creates a new instance out of it and pass that instance off to runApp()
+  runApp(App());
 }
