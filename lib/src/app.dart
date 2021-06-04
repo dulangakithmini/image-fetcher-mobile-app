41065dd20b452de4df0f3b717b41893fad135f07
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-05 05:00:54 +0530

    Create build function inside the App class

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 
 // Create a class for custom widget. This should extend the StatelessWidget base class
-class App extends StatelessWidget {}
-
-// Define a build method that returns the widgets that this widget will show
+class App extends StatelessWidget {
+  // Define a build method that returns the widgets that this widget will show
+  Widget build(context) {}
+}
