ae3c79fa504dcda5b70e56515233ab9eaefd254a
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-05 06:02:39 +0530

    Implement createState function in App class, returning an instance of AppState

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,7 +2,15 @@
 import 'package:flutter/material.dart';
 
 // Create App class extending StatefulWidget in order to refactor Stateless into Stateful
-class App extends StatefulWidget {}
+class App extends StatefulWidget {
+  // Implement createState
+  @override
+  State<StatefulWidget> createState() {
+    // Return an instance of AppState
+    return AppState();
+    throw UnimplementedError();
+  }
+}
 
 // Create AppState class extending State<App>
 class AppState extends State<App> {
