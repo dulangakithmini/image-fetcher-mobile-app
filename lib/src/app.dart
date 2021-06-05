facda0cf4d90b12a8a9188642cad47552011fda4
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-05 05:52:56 +0530

    Create App class extending StatefulWidget in order to refactor Stateless into Stateful

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,9 @@
 // Import flutter helper library
 import 'package:flutter/material.dart';
 
+// Create App class extending StatefulWidget in order to refactor Stateless into Stateful
+class App extends StatefulWidget {}
+
 // Create a class for custom widget. This should extend the StatelessWidget base class
 class App extends StatelessWidget {
   // Define a build method that returns the widgets that this widget will show
