09cfdd641dc9935a46ca1caa557ec2d50bee3fe3
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-05 05:56:05 +0530

    Create AppState class extending State<App>

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -4,8 +4,8 @@ import 'package:flutter/material.dart';
 // Create App class extending StatefulWidget in order to refactor Stateless into Stateful
 class App extends StatefulWidget {}
 
-// Create a class for custom widget. This should extend the StatelessWidget base class
-class App extends StatelessWidget {
+// Create AppState class extending State<App>
+class AppState extends State<App> {
   // Define a build method that returns the widgets that this widget will show
   Widget build(context) {
     return MaterialApp(
@@ -23,3 +23,23 @@ class App extends StatelessWidget {
     );
   }
 }
+
+// Create a class for custom widget. This should extend the StatelessWidget base class
+// class App extends StatelessWidget {
+// Define a build method that returns the widgets that this widget will show
+// Widget build(context) {
+//   return MaterialApp(
+//     home: Scaffold(
+//       appBar: AppBar(
+//         title: Text('Let\'s see some images!'),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {
+//           print('Hi');
+//         },
+//         child: Icon(Icons.add),
+//         ),
+//       ),
+//     );
+//   }
+// }
