2f475f96c574323f5ed884377f2d8ef852c23623
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-06 06:02:37 +0530

    Annotate parsedJson with a type

--- lib/src/models/image_model.dart
+++ lib/src/models/image_model.dart
@@ -6,7 +6,7 @@ class ImageModel {
   ImageModel(this.id, this.url, this.title);
 
   // When passing a json to json.decode(), we get a map with key type String and value type dynamic
-  ImageModel.fromJson(parsedJson) {
+  ImageModel.fromJson(Map<String, dynamic> parsedJson) {
     id = parsedJson['id'];
     url = parsedJson['url'];
     title = parsedJson['title'];
