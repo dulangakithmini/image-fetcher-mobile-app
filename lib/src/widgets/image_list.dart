5d58d8ee856f6e2254c59dce3e768d07170c0a5f
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-10 01:48:20 +0530

    Create the ImageList constructor, expecting List<ImageModel> type parameter

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:pics/src/models/image_model.dart';
 
 /// ImageList does not change the list of images and re-render itself. Its is changed by AppState. Therefore ImageList is a StatelessWidget
-class ImageList extends StatelessWidget {}
+class ImageList extends StatelessWidget {
+  List<ImageModel> images;
+
+  ImageList(this.images);
+}
