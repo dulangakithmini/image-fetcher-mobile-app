03268e514ad9b0fb5ce6cd5828465c711e298625
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-10 02:25:27 +0530

    Make the images List final

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -3,7 +3,7 @@ import 'package:pics/src/models/image_model.dart';
 
 /// ImageList does not change the list of images and re-render itself. Its is changed by AppState. Therefore ImageList is a StatelessWidget
 class ImageList extends StatelessWidget {
-  List<ImageModel> images;
+  final List<ImageModel> images;
 
   ImageList(this.images);
 }
